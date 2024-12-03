01312391e84f73589d4361268f3b97e8718b297c
Matthias Schippling <matthias@schippling.dev>
2024-12-03 17:04:58 +0100

    day 2 solved partially, day 3 solved

---
+++ common.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	s "strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func IsSameSign(x int, y int) bool {
+	return (x >= 0) == (y >= 0)
+}
+
+func RemoveIndex(s []string, index int) []string {
+	ret := make([]string, 0, len(s)-1)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
+}
+
+func GetInputData(path string) []string {
+	var input, err = os.ReadFile(path)
+	check(err)
+
+	var lines = s.Split(string(input), "\n")
+	return lines
+}
